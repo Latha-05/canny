33b8870a88d94fd6ce826885977aacffe75d072b
Latha-05 <113928389+Latha-05@users.noreply.github.com>
2024-10-07 14:40:33 +0530

    Update appln.js

--- appln.js
+++ appln.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 80;
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}`);
 });
